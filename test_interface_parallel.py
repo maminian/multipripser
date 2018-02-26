f9a7ebc6d1d4bb77d653d10f3029a375356a3eeb
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2018-02-26 09:35:11 -0700

    cleaner parallelization

--- test_interface_parallel.py
+++ test_interface_parallel.py
@@ -6,6 +6,8 @@ import numpy as np
 from matplotlib import pyplot
 import multiprocessing
 
+fpref = ri.generate_unique_id()
+
 n = 1001
 t = np.linspace(0, 2*np.pi, n, endpoint=False)
 rawcurve = np.zeros((n,3))
@@ -15,13 +17,14 @@ for i,tv in enumerate(t):
 
 
 def PH_realization(inv):
+    fname = fpref + '_' + str(inv).zfill(4) + '.txt'
     cloud = np.zeros((n,3))
     cloud = rawcurve + 0.2*np.random.randn(rawcurve.shape[0],rawcurve.shape[1])
-    PH_intervals = ri.run_ripser_sim(cloud)
+    PH_intervals = ri.run_ripser_sim(cloud,fname=fname)
     print(inv)
     return PH_intervals
 #
 
-p = multiprocessing.Pool(4)
+p = multiprocessing.Pool(12)
 
-results = p.map(PH_realization,[i for i in range(8)])
+results = p.map(PH_realization,[i for i in range(24)])
