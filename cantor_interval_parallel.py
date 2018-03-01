90159a9740d89a793b8a9e2fc6638691964b55b8
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2018-03-01 13:44:12 -0700

    bleh

--- cantor_interval_parallel.py
+++ cantor_interval_parallel.py
@@ -34,19 +34,38 @@ def PH_realization(inval):
     return i,PH_intervals
 #
 
-nproc = 4
+nproc = 60
 nmin = 20
-nmax = 200
-dn = 20
+nmax = 1000
+#dn = 20
 
 reps = 100
 
+# samples = np.arange(nmin,nmax+1,dn, dtype=int)
+# coeffs = np.arange(1,10,0.5)
+# powers = 10.**np.arange(4)
+#nc = len(coeffs)
+#npow = len(powers)
+#coeffs.shape = (nc,1)
+#powers.shape = (1,npow)
+#samples = np.dot(coeffs,powers).flatten()
+#samples = samples[samples>=nmin]
+#samples = samples[samples<=nmax]
+
+samples = 2**np.arange(5,13+1)
+
+samples = np.array(samples, dtype=int)
+print(samples)
+print(len(samples))
+
 p = multiprocessing.Pool(nproc)
 
-samples = np.arange(nmin,nmax+1,dn, dtype=int)
 samples = np.array([[[sample,rep] for sample in samples] for rep in np.arange(reps)])
 samples = np.reshape(samples, (samples.shape[0]*samples.shape[1],2) )
 
+print(len(samples)) 
+
 results = p.map(PH_realization,samples)
 
-ri.save_thing(results,'cantor_interval_n%i_reps%i.pkl'%(nmax,reps))
+#ri.save_thing(results,'cantor_interval_n%i_reps%i.pkl'%(nmax,reps))
+ri.save_thing(results,'cantor_interval_n%i_reps%i_2pow.pkl'%(np.array(samples).max(),reps))
