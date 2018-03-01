3cadc17eda576cf41600cfd42168edcfd34beeeb
maminian <manuchehr.aminian@gmail.com>
2018-03-01 15:26:34 -0700

    misc

--- display_cantor_pows.py
+++ display_cantor_pows.py
@@ -4,8 +4,9 @@ import ripser_interface as ri
 import numpy as np
 from matplotlib import pyplot
 
-results = ri.load_thing('cantor_interval_n8192_reps100_2pow.pkl')
-d = 1 + np.log(2)/np.log(3)
+results = ri.load_thing('cantor_n8192_reps100.pkl')
+d = np.log(2)/np.log(3)
+# d = 1
 ns = np.array([result[0] for result in results])
 nsu = np.unique(ns)
 colors = pyplot.cm.rainbow(np.linspace(0,1,len(nsu)))
@@ -24,7 +25,9 @@ for result in results:
         birthdeath = result[1][0][:k]
         bars = np.diff(birthdeath,axis=1).flatten()
         order = np.argsort(bars)
-        ax0.step(np.linspace(0,1,k),nv**(1./d)*bars[order],c=cdict[result[0]],lw=1)
+        # ax0.step(np.linspace(0,1,k),nv**(1./d)*bars[order],c=cdict[result[0]],lw=1)
+        ax0.step(np.linspace(0,1,k),bars[order],c=cdict[result[0]],lw=1)
+        print(bars[order[-1]])
     #
 #
 
@@ -33,31 +36,7 @@ for nsuu in nsu:
 #
 
 ax0.legend(loc='upper left')
-fig0.suptitle(r'$PH^0$ for the set $C \times [0,1]$, points sampled from $2^i$')
-
-######################
-#
-# PH^1
-#
-fig1,ax1 = pyplot.subplots(1,1)
-
-for result in results:
-    k = len(result[1][1])-1
-    if k>1:
-        nv = result[0]
-        birthdeath = result[1][1][:k]
-        bars = np.diff(birthdeath,axis=1).flatten()
-        order = np.argsort(bars)
-        ax1.step(np.linspace(0,1,k),nv**(1./d)*bars[order],c=cdict[result[0]],lw=1)
-    #
-#
-
-for nsuu in nsu:
-    ax1.plot([],[],c=cdict[nsuu],lw=2,label=r'$n=%i$'%nsuu)
-#
-
-ax1.legend(loc='upper left')
-fig1.suptitle(r'$PH^1$ for the set $C \times [0,1]$, points sampled from $2^i$')
+fig0.suptitle(r'$PH^0$ for the Cantor set, points sampled from $2^i$')
 
 
 pyplot.show(block=False)
