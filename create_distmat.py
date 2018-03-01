4eb0ddff4730f932a853ae2aae6976a15ac49f2a
maminian <manuchehr.aminian@gmail.com>
2018-03-01 12:03:33 -0700

    Major speedup using sklearn distance matrix code

--- create_distmat.py
+++ create_distmat.py
@@ -3,14 +3,16 @@ def create_distmat(cloud):
     '''
     Create distance matrix. outputs np array.
     '''
-    import numpy as np
+    # import numpy as np
+    import sklearn.metrics
 
-    n = len(cloud)
-    D = np.zeros((n,n))
-
-    for i in range(n):
-        for j in range(i):
-            D[i,j] = np.linalg.norm(cloud[i]-cloud[j])
+    # n = len(cloud)
+    # D = np.zeros((n,n))
     #
+    # for i in range(n):
+    #     for j in range(i):
+    #         D[i,j] = np.linalg.norm(cloud[i]-cloud[j])
+    # #
+    D = sklearn.metrics.pairwise.pairwise_distances(cloud)
     return D
 #
