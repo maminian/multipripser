15925383c6f544024d330a10cf478b899ad61179
maminian <manuchehr.aminian@gmail.com>
2018-08-02 20:00:03 -0600

    support for reading point cloud outputs from ripser, still a one-off fix

--- read_ripser_results.py
+++ read_ripser_results.py
@@ -1,4 +1,4 @@
-def read_ripser_results(fname):
+def read_ripser_results(fname,**kwargs):
     '''
     Reads the raw output of ripser and returns
     a data structure with the results in a useable format.
@@ -9,6 +9,8 @@ def read_ripser_results(fname):
     import re
     import numpy as np
 
+    input_type = kwargs.get('input_type', 'distance matrix')
+
     if type(fname)==str:
         f = open(fname,'r')
         lines = f.readlines()
@@ -17,7 +19,12 @@ def read_ripser_results(fname):
         lines = fname
     #
 
-    expr_header = 'distance matrix with ([\d]*) points'
+    if input_type=='distance matrix':
+        expr_header = 'distance matrix with ([\d]*) points'
+    elif input_type=='point cloud':
+        # print(input_type)
+        expr_header = 'point cloud with ([\d]*) points in dimension'
+    #
     expr_PHdim = 'persistence intervals in dim ([\d]):'
     expr_interval = '\ \[(.*),(.*)\)'
 
@@ -32,8 +39,13 @@ def read_ripser_results(fname):
     match = prog_header.match(lines[0])
     n = int(match.group(1))
 
-    for line in lines[2:]:
+    # import pdb
+    # pdb.set_trace()
 
+    for line in lines[2:]:
+        if re.match('value range', line):
+            continue
+        #
         m = prog_PHdim.match(line)
         if m!=None:
             # initialize a new dictionary entry for the new PH dimension
@@ -42,6 +54,12 @@ def read_ripser_results(fname):
         else:
 
             m = prog_interval.match(line)
+            if m==None:
+                print('Warning: could not match the following line: ')
+                print(line)
+                print("It's probably nothing.")
+                continue
+            #
             left = np.double(m.group(1))
             right = np.double(m.group(2)) if m.group(2)!=' ' else np.inf
             PH_intervals[dim].append([left,right])
