8693135af5aebf1f3997de1c9153db0f6e8d477c
maminian <manuchehr.aminian@gmail.com>
2018-08-02 20:20:17 -0600

    relative imports on a different branch

--- ripser_interface.py
+++ ripser_interface.py
@@ -4,13 +4,13 @@
 # interface file.
 #
 
-from create_distmat_file import create_distmat_file as create_ripser_file
-from create_distmat import create_distmat
-from create_distmat_str import create_distmat_str
-from read_ripser_results import read_ripser_results
-from run_ripser_sim import run_ripser_sim
-from ripser_misc import *
-from gen_pt_cloud_from_measure import *
-import test_measures
-from single_sequence_scaling import *
-import l_systems
+from .create_distmat_file import create_distmat_file as create_ripser_file
+from .create_distmat import create_distmat
+from .create_distmat_str import create_distmat_str
+from .read_ripser_results import read_ripser_results
+from .run_ripser_sim import run_ripser_sim
+from .ripser_misc import *
+from .gen_pt_cloud_from_measure import *
+from . import test_measures
+from .single_sequence_scaling import *
+from . import l_systems
