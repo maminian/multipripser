d9a65e9477e6f9660536bcb4b492169117d4520a
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2019-03-04 11:42:59 -0700

    example with more than one-dimensional homology

--- test_interface.py
+++ test_interface.py
@@ -5,16 +5,21 @@ import ripser_interface as ri
 import numpy as np
 from matplotlib import pyplot
 
-n = 1001
-t = np.linspace(0,2*np.pi,n, endpoint=False)
+n = 201
+t = np.linspace(0,2*np.pi,n)
 cloud = np.zeros((n,3))
 
+# Make a cloud of points in a "barrel"; spherical polar coordinate 
+# restricted to the equator region.
 for i,tv in enumerate(t):
-    cloud[i] = [np.cos(tv),np.sin(tv),np.sqrt(2.)/3.*np.cos(3*tv)]
-    cloud[i] += 0.2*np.random.randn(3)
+    phi = np.pi/4 + np.pi/2*np.random.rand()
+    cloud[i] = [np.cos(tv)*np.sin(phi),np.sin(tv)*np.sin(phi), np.cos(phi) ]
+    cloud[i] += 0.05*np.random.randn(3)
 #
 
-PH_intervals = ri.run_ripser_sim(cloud)
+PH_intervals = ri.run_ripser_sim(cloud, max_dim=2)
 
-fig0,ax0 = ri.plot_PH_results(PH_intervals[0])
-fig1,ax1 = ri.plot_PH_results(PH_intervals[1])
+#fig0,ax0 = ri.plot_PH_results(PH_intervals[0])
+#fig1,ax1 = ri.plot_PH_results(PH_intervals[1])
+
+fig,ax = ri.plot_PH_summary(PH_intervals)
